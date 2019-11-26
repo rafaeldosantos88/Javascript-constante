a3f89b504d856feb1898585aacf56e800e7a2345
Rafael Dos Santos <raphael_979@hotmail.com>
2019-11-26 16:33:38 -0300

    Usando constante

---
+++ constante.js
@@ -0,0 +1,16 @@
+const nome = 'Rafael';  //Na constante não podemos modificar o valor,devemos iniciar com um valor
+let nome1='Rafael Santos';
+const primeiroNumero=2020;
+const segundoNumero=1993;
+const conta= primeiroNumero - segundoNumero ;
+
+console.log(nome);
+
+console.log('______________________________________________');
+console.log(nome1);
+console.log('______________________________________________');
+console.log(conta,'anos');
+console.log('______________________________________________');
+console.log('_______Caso quero saber o tipo variavel_______');
+console.log(typeof primeiroNumero); 
+console.log('______________________________________________'); //Aqui  coloqui o Typeoff para saber tipagem
